997a3e6180105884b2219734784a274a78e02718
smandekar1 <smandekar1@yahoo.com>
2015-09-05 21:33:45 -0500

    styles, bootstrap added

--- SimpleBlog/Areas/Admin/Controllers/UsersController.cs
+++ SimpleBlog/Areas/Admin/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleBlog.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,12 +9,12 @@ namespace SimpleBlog.Areas.Admin.Controllers
 {
 
     [Authorize(Roles = "admin")]
+    [SelectedTab("users")]
     public class UsersController : Controller
     {
-        // GET: Admin/Users
         public ActionResult Index()
         {
-            return Content("Users!!");
+            return View();
         }
     }
 }
