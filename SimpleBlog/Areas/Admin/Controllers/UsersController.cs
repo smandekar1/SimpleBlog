cff679c3410359ae6f09185897e9bd17642eba48
smandekar1 <smandekar1@yahoo.com>
2015-09-16 20:59:35 -0500

    prevented CSRF

--- SimpleBlog/Areas/Admin/Controllers/UsersController.cs
+++ SimpleBlog/Areas/Admin/Controllers/UsersController.cs
@@ -31,7 +31,7 @@ namespace SimpleBlog.Areas.Admin.Controllers
 
         }
 
-        [HttpPost]
+        [HttpPost, ValidateAntiForgeryToken]
         public ActionResult New(UsersNew form)
         {
             if (Database.Session.Query<User>().Any(u => u.Username == form.Username))
@@ -65,7 +65,7 @@ namespace SimpleBlog.Areas.Admin.Controllers
                 Email = user.Email
             });
         }
-        [HttpPost]
+        [HttpPost, ValidateAntiForgeryToken]
         public ActionResult Edit(int id, UsersEdit form)
         {
             var user = Database.Session.Load<User>(id);
@@ -100,7 +100,7 @@ namespace SimpleBlog.Areas.Admin.Controllers
             });
         } 
     
-        [HttpPost]
+        [HttpPost, ValidateAntiForgeryToken]
         public ActionResult ResetPassword(int id, UsersResetPassword form)
         {
             var user = Database.Session.Load<User>(id);
@@ -119,7 +119,7 @@ namespace SimpleBlog.Areas.Admin.Controllers
 
         }
 
-      
+        [HttpGet, ValidateAntiForgeryToken]
         public ActionResult Delete(int id)
         {
             var user = Database.Session.Load<User>(id);
