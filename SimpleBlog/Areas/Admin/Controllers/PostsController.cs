9cbdb34471db181ee9671620f4b8ff0991e0d4e2
smandekar1 <smandekar1@yahoo.com>
2015-09-21 12:01:37 -0500

    changed create user form and detele toggle button

--- SimpleBlog/Areas/Admin/Controllers/PostsController.cs
+++ SimpleBlog/Areas/Admin/Controllers/PostsController.cs
@@ -1,4 +1,7 @@
-﻿using SimpleBlog.Infrastructure;
+﻿using NHibernate.Linq;
+using SimpleBlog.Areas.Admin.ViewModels;
+using SimpleBlog.Infrastructure;
+using SimpleBlog.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,16 +9,36 @@ using System.Web;
 using System.Web.Mvc;
 
 namespace SimpleBlog.Areas.Admin.Controllers
-    
+
 {
     [Authorize(Roles = "admin")]
     [SelectedTab("posts")]
-    public class PostsController: Controller
-        {
+    public class PostsController : Controller
+    {
+        private const int PostsPerPage = 5;
 
-            public ActionResult Index()
+        public ActionResult Index(int page = 1)
+        {
+            var totalPostCount = Database.Session.Query<Post>().Count();
+
+            var currentPostPage = Database.Session.Query<Post>()
+                .OrderByDescending(c => c.CreatedAt)
+                .Skip((page - 1) * PostsPerPage)
+                .Take(PostsPerPage)
+                .ToList();
+
+            return View(new PostsIndex
             {
-            return View();
-            }
+                Posts = new PagedData<Post>(currentPostPage, totalPostCount, page, PostsPerPage)
+            });
+
+        }
+        public ActionResult New()
+        {
+            return View("Form", new PostsForm
+            {
+                IsNew = true
+            });
+        }
     }
 }
